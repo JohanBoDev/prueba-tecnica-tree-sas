8d10f28745e9f39242628e705429e93cdedd778c
JohanBoDev <bohorquezjohan958@gmail.com>
2025-06-07 12:33:28 -0500

    .

--- backend/src/validators/idValidator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validarIdMongo = void 0;
-const express_validator_1 = require("express-validator");
-exports.validarIdMongo = [
-    (0, express_validator_1.param)('id').isMongoId().withMessage('ID no válido')
-];
